27de22caca4fbcd19c0ae7d221d0614f80f7df8e
WEN-we <19892142453@139.com>
2026-09-08 13:06:29 +0800

    release: v1.2.2(versionCode10)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.spotifytools.lyrics"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
+        versionCode = 10
         versionName = "1.2.2"
     }
 
